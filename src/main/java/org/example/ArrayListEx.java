b4f7a7a30215cfe647df8b1bc33ef386eda0324e
AlinIulian97 <alin_iulia_2007@yahoo.com>
2023-02-27 15:49:04 +0200

    Array

---
+++ src/main/java/org/example/ArrayListEx.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ArrayListEx {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        ArrayList[] a = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            int  d = scanner.nextInt();
+            a[i] = new ArrayList<>();
+            for (int j=0; j<d; j++){
+                a[i].add(scanner.nextInt());
+            }
+        }
+        int q = scanner.nextInt();
+        for(int i = 0 ; i<q ; i++){
+            int x = scanner.nextInt();
+            int y= scanner.nextInt();
+            try{
+                System.out.println(a[x-1].get(y-1));
+            }catch (Exception e){
+                System.out.println("ERROR!");
+            }
+        }
+    }
+}
